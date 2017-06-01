46344bcca04d7c4509e21bf37cc7e458a8137a4c
datsyuk <yan.datsiuk@thinkmobiles.com>
2017-06-01 16:28:50 +0300

    Added seeders If applied, commit will add seeders

--- database/seeds/ApartmentTypesSeeder.php
+++ database/seeds/ApartmentTypesSeeder.php
@@ -11,6 +11,44 @@ class ApartmentTypesSeeder extends Seeder
      */
     public function run()
     {
-        //
+        \App\REST\ApartmentType::firstOrCreate([
+            'name' => 'CONVERTIBLE'
+        ]);
+
+        \App\REST\ApartmentType::firstOrCreate([
+            'name' => 'STUDIO'
+        ]);
+
+        \App\REST\ApartmentType::firstOrCreate([
+            'name' => 'CONVERTIBLE STUDIO'
+        ]);
+
+        \App\REST\ApartmentType::firstOrCreate([
+            'name' => 'ALCOVE STUDIO'
+        ]);
+
+        \App\REST\ApartmentType::firstOrCreate([
+            'name' => 'LOFT'
+        ]);
+
+        \App\REST\ApartmentType::firstOrCreate([
+            'name' => 'TWO-BEDROOM'
+        ]);
+
+        \App\REST\ApartmentType::firstOrCreate([
+            'name' => 'THREE-ROOM'
+        ]);
+
+        \App\REST\ApartmentType::firstOrCreate([
+            'name' => 'DUPLEX'
+        ]);
+
+        \App\REST\ApartmentType::firstOrCreate([
+            'name' => 'TRIPLEX'
+        ]);
+
+        \App\REST\ApartmentType::firstOrCreate([
+            'name' => 'GARDEN APARTMENT'
+        ]);
     }
 }
--- database/seeds/FacilitiesSeeder.php
+++ database/seeds/FacilitiesSeeder.php
@@ -11,6 +11,60 @@ class FacilitiesSeeder extends Seeder
      */
     public function run()
     {
-        //
+        \App\REST\Facility::firstOrCreate([
+            'description' => 'Wireless Internet'
+        ]);
+
+        \App\REST\Facility::firstOrCreate([
+            'description' => 'Kitchen'
+        ]);
+
+        \App\REST\Facility::firstOrCreate([
+            'description' => 'Heating'
+        ]);
+
+        \App\REST\Facility::firstOrCreate([
+            'description' => 'TV'
+        ]);
+
+        \App\REST\Facility::firstOrCreate([
+            'description' => 'Wagter'
+        ]);
+
+        \App\REST\Facility::firstOrCreate([
+            'description' => 'Intercom'
+        ]);
+
+        \App\REST\Facility::firstOrCreate([
+            'description' => 'Breakfast'
+        ]);
+
+        \App\REST\Facility::firstOrCreate([
+            'description' => 'Internet'
+        ]);
+
+        \App\REST\Facility::firstOrCreate([
+            'description' => 'Cable TV'
+        ]);
+
+        \App\REST\Facility::firstOrCreate([
+            'description' => 'Fireplace'
+        ]);
+
+        \App\REST\Facility::firstOrCreate([
+            'description' => 'Air conditioning'
+        ]);
+
+        \App\REST\Facility::firstOrCreate([
+            'description' => 'A place to work on a laptop'
+        ]);
+
+        \App\REST\Facility::firstOrCreate([
+            'description' => 'Washer'
+        ]);
+
+        \App\REST\Facility::firstOrCreate([
+            'description' => 'Iron'
+        ]);
     }
 }

c801d8688bb54dc86d771b211dd259f7e2ab138e
datsyuk <yan.datsiuk@thinkmobiles.com>
2017-06-01 16:29:36 +0300

    Added seeders If applied, commit will add seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-
 		$this->call(\TMPHP\RestApiGenerators\Database\Seeds\AuthActionsTableSeeder::class);
 		$this->call(\TMPHP\RestApiGenerators\Database\Seeds\AuthGroupsTableSeeder::class);
 		$this->call(\TMPHP\RestApiGenerators\Database\Seeds\AuthActionGroupsTableSeeder::class);
